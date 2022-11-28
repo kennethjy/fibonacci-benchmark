1181021c87605f0d6d00787e761bce57c94530fe
kennethjy <114073455+kennethjy@users.noreply.github.com>
2022-11-28 18:02:17 +0700

    Update main_b_space_recursive.c

--- main_b_space_recursive.c
+++ main_b_space_recursive.c
@@ -2,7 +2,7 @@
 #include "mylib/mylib.h"
 
 int main(void){
-    int n = 1
+    int n = 1;
     while(n < 2){
         for(int i = -1; i <= 1000; i++){
             printf("%d, %d\n", recursive(i));
